232fd36d1a8e6f0d89ba9e7ef3917f78c6ed19ee
Amoenus <motivated.it@gmail.com>
2015-01-14 00:49:51 +0000

    WeatherContract cleanup

--- app/src/main/java/app/amoenus/traktroulette/data/WeatherContract.java
+++ app/src/main/java/app/amoenus/traktroulette/data/WeatherContract.java
@@ -36,18 +36,15 @@ public class WeatherContract {
 
     // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
     // the content provider.
-
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
     // Possible paths (appended to base content URI for possible URI's)
-    // For instance, content://app.amoenus.traktroulette/weather/ is a valid path for
-    // looking at weather data. content://app.amoenus.traktroulette/givemeroot/ will fail,
+    // For instance, content://com.example.android.sunshine.app/weather/ is a valid path for
+    // looking at weather data. content://com.example.android.sunshine.app/givemeroot/ will fail,
     // as the ContentProvider hasn't been given any information on what to do with "givemeroot".
     // At least, let's hope not.  Don't be that dev, reader.  Don't be that dev.
-
     public static final String PATH_WEATHER = "weather";
     public static final String PATH_LOCATION = "location";
-
 
     // Format used for storing dates in the database.  ALso used for converting those strings
     // back into date objects for comparison/processing.
@@ -58,8 +55,6 @@ public class WeatherContract {
      * @param date The input date
      * @return a DB-friendly representation of the date, using the format defined in DATE_FORMAT.
      */
-
-
     public static String getDbDateString(Date date){
         // Because the API returns a unix timestamp (measured in seconds),
         // it must be converted to milliseconds in order to be converted to valid date.
@@ -72,8 +67,6 @@ public class WeatherContract {
      * @param dateText the input date string
      * @return the Date object
      */
-
-
     public static Date getDateFromDb(String dateText) {
         SimpleDateFormat dbDateFormat = new SimpleDateFormat(DATE_FORMAT);
         try {
@@ -84,9 +77,17 @@ public class WeatherContract {
         }
     }
 
-
+    /* Inner class that defines the table contents of the location table */
     public static final class LocationEntry implements BaseColumns {
-        /* Inner class that defines the table contents of the location table */
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
+
+        public static final String CONTENT_TYPE =
+                "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+        public static final String CONTENT_ITEM_TYPE =
+                "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+
         // Table name
         public static final String TABLE_NAME = "location";
 
@@ -103,24 +104,21 @@ public class WeatherContract {
         public static final String COLUMN_COORD_LAT = "coord_lat";
         public static final String COLUMN_COORD_LONG = "coord_long";
 
-        public static final Uri CONTENT_URI =
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
-
-        public static final String CONTENT_TYPE =
-                "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
-        public static final String CONTENT_ITEM_TYPE =
-                "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
-
-        public static Uri buildLocationUri(long id)
-        {
-            Uri result = ContentUris.withAppendedId(CONTENT_URI, id);
-            return result;
+        public static Uri buildLocationUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
         }
     }
 
-/*
-/* Inner class that defines the table contents of the weather table */
+    /* Inner class that defines the table contents of the weather table */
     public static final class WeatherEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_WEATHER).build();
+
+        public static final String CONTENT_TYPE =
+                "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
+        public static final String CONTENT_ITEM_TYPE =
+                "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
 
         public static final String TABLE_NAME = "weather";
 
@@ -151,13 +149,6 @@ public class WeatherContract {
         // Degrees are meteorological degrees (e.g, 0 is north, 180 is south).  Stored as floats.
         public static final String COLUMN_DEGREES = "degrees";
 
-        public static final Uri CONTENT_URI =
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_WEATHER).build();
-
-        public static final String CONTENT_TYPE =
-                "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
-        public static final String CONTENT_ITEM_TYPE =
-                "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
 
         public static Uri buildWeatherUri(long id) {
             return ContentUris.withAppendedId(CONTENT_URI, id);
@@ -187,21 +178,6 @@ public class WeatherContract {
 
         public static String getStartDateFromUri(Uri uri) {
             return uri.getQueryParameter(COLUMN_DATETEXT);
-        }
-
-        /**
-         * Converts a dateText to a long Unix time representation
-         * @param dateText the input date string
-         * @return the Date object
-         */
-        public static Date getDateFromDb(String dateText) {
-            SimpleDateFormat dbDateFormat = new SimpleDateFormat(DATE_FORMAT);
-            try {
-                return dbDateFormat.parse(dateText);
-            } catch ( ParseException e ) {
-                e.printStackTrace();
-                return null;
-            }
         }
     }
 }
